965ef0b4b28628864f32ed09c235de6bf7e1717d
BiYeon <isly7959@naver.com>
2024-03-22 17:08:14 +0900

    initial commit

---
+++ src/main/java/com/capstone/crmproject/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.capstone.crmproject;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CrMprojectApplication.class);
+    }
+
+}
---
+++ src/test/java/com/capstone/crmproject/CrMprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.crmproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrMprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
